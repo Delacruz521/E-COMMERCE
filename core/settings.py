aa45e77712840ac29b300dc17f177b77612d2669
Karl <115046154+Delacruz521@users.noreply.github.com>
2025-08-23 18:56:20 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 # Smtp
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "karlanthonydelacruz128@gmail.com"
-EMAIL_HOST_PASSWORD = "mvcd pejr zmbn wfmj"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND  = "django.core.mail.backends.smtp.EmailBackend"
